301d1ef9e6ee774537cc1a0f031ebcb4208d6d60
Kaylin Dennis <kaylinjdennis@gmail.com>
2021-02-03 20:54:56 -0700

    takes in an object and a value and returns the appropriate key

---
+++ findKeyByValue.js
@@ -0,0 +1,28 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`💚💚💚 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`💔💔💔 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKeyByValue = function(object, value) {
+  let keysArr = Object.keys(object);
+  for (let key of keysArr) {
+    if (object[key] === value) {
+      return key;
+    }
+  }
+};
+
+
+
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama: "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+assertEqual(findKeyByValue(bestTVShowsByGenre, "Brooklyn Nine-Nine"), "comedy");
