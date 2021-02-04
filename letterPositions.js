a1e53aab70080990e5a473c48927613c0a483dfd
Kaylin Dennis <kaylinjdennis@gmail.com>
2021-02-03 20:12:56 -0700

    return an object containing arrays of the indices where each letter occurs in a given string

---
+++ letterPositions.js
@@ -0,0 +1,38 @@
+const assertArraysEqual = function(arr1, arr2) {
+  if (eqArrays(arr1, arr2)) {
+    console.log(`💚💚💚 Assertion Passed: ${arr1} === ${arr2}`);
+  } else {
+    console.log(`💔💔💔 Assertion Failed: ${arr1} !== ${arr2}`);
+  }
+};
+
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const letterPositions = function(sentence) {
+  const results = {};
+  for (let i = 0; i < sentence.length; i++) {
+    if (sentence[i] !== ' ') {
+      if (!results[sentence[i]]) {
+        results[sentence[i]] = [];
+      }
+      results[sentence[i]].push(i);
+    }
+  }
+  return results;
+};
+
+let positionsObj = letterPositions('hello');
+assertArraysEqual(positionsObj.h, [0]);
+assertArraysEqual(positionsObj.e, [1]);
+assertArraysEqual(positionsObj.l, [2, 3]);
+assertArraysEqual(positionsObj.o, [4]);
