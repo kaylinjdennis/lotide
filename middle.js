fde194c45630ab6d91475c8eda78940dfcb7fab6
Kaylin Dennis <kaylinjdennis@gmail.com>
2021-02-08 13:59:18 -0700

    refactored to separate tests

--- middle.js
+++ middle.js
@@ -1,23 +1,3 @@
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-
-const assertArraysEqual = function(arr1, arr2) {
-  if (eqArrays(arr1, arr2)) {
-    console.log(`💚💚💚 Assertion Passed: ${arr1} === ${arr2}`);
-  } else {
-    console.log(`💔💔💔 Assertion Failed: ${arr1} !== ${arr2}`);
-  }
-};
-
 const middle = function(array) {
   let newArr = [];
   if (array.length > 2) {
@@ -31,9 +11,4 @@ const middle = function(array) {
   return newArr;
 };
 
-assertArraysEqual(middle([1]), []);
-assertArraysEqual(middle([1, 2]), []);
-assertArraysEqual(middle([1, 2, 3]), [2]);
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+module.exports = middle;
