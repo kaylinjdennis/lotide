f30f7191cc969fe99264fbc5562ff7a792f90ddf
Kaylin Dennis <kaylinjdennis@gmail.com>
2021-02-08 13:29:59 -0700

    changed from copy/pasting function to exporting module and requiring, and created test files

--- assertEqual.js
+++ assertEqual.js
@@ -6,11 +6,4 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-// Test Code
-assertEqual('Lighthouse Labs', 'Bootcamp');
-assertEqual(1, 1);
-
-assertEqual('Hello', 'Hello');
-assertEqual('Hello', 'hello');
-assertEqual(7, 15);
-assertEqual(4, 4);
+module.exports = assertEqual;
--- head.js
+++ head.js
@@ -1,11 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`💚💚💚 Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`💔💔💔 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const head = function(array) {
   if (array.length > 0) {
     return array[0];
@@ -14,8 +6,4 @@ const head = function(array) {
   }
 };
 
-// Test Code
-assertEqual(head([5, 6, 7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([17]), 17);
-assertEqual(head([]), undefined);
+module.exports = head;
