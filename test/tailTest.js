6bac690fe16bd295c5997b3660cf83d8289daf00
Kaylin Dennis <kaylinjdennis@gmail.com>
2021-02-08 14:53:07 -0700

    refactored to use mocha and chai for tests

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,17 +1,17 @@
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 const tail = require('../tail');
 
-const result1 = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result1.length, 2);
-assertEqual(result1[0], "Lighthouse");
-assertEqual(result1[1], "Labs");
-
-const words = ['Hello', 'World', 'Goodbye'];
-tail(words);
-assertEqual(words.length, 3);
-
-const result2 = tail([5]);
-assertEqual(result2.length, 0);
-
-const result3 = tail([]);
-assertEqual(result3.length, 0);
+describe('#tail', () => {
+  it('returns ["Lighthouse", "Labs"] when passed ["Hello", "Lighthouse", "Labs"]', () => {
+    assert.deepEqual(tail(["Hello", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"]);
+  });
+  it('does not change original array', () => {
+    const words = ['Hello', 'World', 'Goodbye'];
+    tail(words);
+    assert.deepEqual(words, ['Hello', 'World', 'Goodbye']);
+  });
+  it('returns empty array if passed array of less than two elements', () => {
+    assert.deepEqual(tail([5]), []);
+    assert.deepEqual(tail([]), []);
+  });
+});
